99a7b3b908ac27dc7865f7bc44406805c63352bb
czachariah <zachariahchris@yahoo.com>
2020-11-13 18:58:01 -0500

    small update

--- ML/yeah-world/run.py
+++ ML/yeah-world/run.py
@@ -20,6 +20,8 @@ from os import getenv
 
 import numpy as np
 from tensorflow import keras
+import picamera
+import time
 
 from camera import Camera
 from pinet import PiNet
@@ -35,13 +37,15 @@ if SHOW_UI:
 
 
 def main():
-    if len(argv) != 2 or argv[1] == '--help':
-        print("""Usage: run.py MODEL
+    if len(argv) != 3 or argv[1] == '--help':
+        print("""Usage: run.py MODEL token
 Use MODEL to classify camera frames and play sounds when class 0 is recognised.""")
         exit(1)
 
     model_file = argv[1]
-
+    
+    print(argv[2])
+    
     # We use the same MobileNet as during recording to turn images into features
     print('Loading feature extractor')
     extractor = PiNet()
@@ -52,14 +56,7 @@ Use MODEL to classify camera frames and play sounds when class 0 is recognised."
 
     # Initialize the camera and sound systems
     camera = Camera(training_mode=False)
-    random_sound = RandomSound()
-
-
-    # Create a preview window so we can see if we are in frame or not
-    if SHOW_UI:
-        pygame.display.init()
-        pygame.display.set_caption('Loading')
-        screen = pygame.display.set_mode((512, 512))
+    #random_sound = RandomSound()
 
     # Smooth the predictions to avoid interruptions to audio
     smoothed = np.ones(classifier.output_shape[1:])
@@ -67,41 +64,36 @@ Use MODEL to classify camera frames and play sounds when class 0 is recognised."
 
     print('Now running!')
     while True:
-        raw_frame = camera.next_frame()
+        while True:
+            raw_frame = camera.next_frame()
 
-        # Use MobileNet to get the features for this frame
-        z = extractor.features(raw_frame)
+            # Use MobileNet to get the features for this frame
+            z = extractor.features(raw_frame)
 
-        # With these features we can predict a 'normal' / 'yeah' class (0 or 1)
-        # Keras expects an array of inputs and produces an array of outputs
-        classes = classifier.predict(np.array([z]))[0]
+            # With these features we can predict a 'normal' / 'yeah' class (0 or 1)
+            # Keras expects an array of inputs and produces an array of outputs
+            classes = classifier.predict(np.array([z]))[0]
 
-        # smooth the outputs - this adds latency but reduces interruptions
-        smoothed = smoothed * SMOOTH_FACTOR + classes * (1.0 - SMOOTH_FACTOR)
-        selected = np.argmax(smoothed) # The selected class is the one with highest probability
+            # smooth the outputs - this adds latency but reduces interruptions
+            smoothed = smoothed * SMOOTH_FACTOR + classes * (1.0 - SMOOTH_FACTOR)
+            selected = np.argmax(smoothed) # The selected class is the one with highest probability
 
-        # Show the class probabilities and selected class
-        summary = 'Class %d [%s]' % (selected, ' '.join('%02.0f%%' % (99 * p) for p in smoothed))
-        stderr.write('\r' + summary)
+            # Show the class probabilities and selected class
+            summary = 'Class %d [%s]' % (selected, ' '.join('%02.0f%%' % (99 * p) for p in smoothed))
+            stderr.write('\r' + summary)
 
-        # Perform actions for selected class. In this case, play a sound from the sounds/ dir
-        if selected == 0:
-            random_sound.play_from('sounds/')
-        else:
-            random_sound.stop()
-
-        # Show the image in a preview window so you can tell if you are in frame
-        if SHOW_UI:
-            pygame.display.set_caption(summary)
-            surface = pygame.surfarray.make_surface(raw_frame)
-            screen.blit(pygame.transform.smoothscale(surface, (512, 512)), (0, 0))
-            pygame.display.flip()
-
-            for evt in pygame.event.get():
-                if evt.type == pygame.QUIT:
-                    pygame.quit()
-                    break
-
-
+            # Perform actions for selected class. In this case, play a sound from the sounds/ dir
+            if selected == 0:
+                print("Detected")
+                break
+        # hand detected, send notifcation
+        dateAndTime = time.asctime( time.localtime(time.time()) )
+        camera.camera.resolution = (1280,720)
+        camera.camera.capture(dateAndTime + ".jpg")
+        camera.camera.resolution = (128, 128)
+        time.sleep(2)
+        
+            
+        
 if __name__ == '__main__':
     main()
